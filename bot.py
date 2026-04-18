d4769b44913b544a5154f68b8fa2186a9291933c
itmyanmarsoe <148292993+itmyanmarsoe@users.noreply.github.com>
2026-04-18 15:42:13 +0630

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,22 +6,25 @@ import os
 
 def send_to_blogger():
     try:
+        # ၁။ အချက်အလက်များ
         gmail_user = "soe41959@gmail.com" 
         gmail_password = os.environ.get('GMAIL_PASSWORD')
         blogger_email = "soe41959.exam2026@blogger.com" 
         
+        # ၂။ JSON Data
         raw_pdf_url = "https://githubusercontent.com/itmyanmarsoe/exam-result-auto/main/result.pdf"
-        result_data = {"years": [{"name": "၂၀၂၆ အောင်စာရင်း (စမ်းသပ်ချက်)", "href": raw_pdf_url}]}
+        result_data = {"years": [{"name": "၂၀၂၆ အောင်စာရင်း (Email စနစ်)", "href": raw_pdf_url}]}
         json_content = json.dumps(result_data, ensure_ascii=False)
 
+        # ၃။ Email ပြင်ဆင်ခြင်း
         msg_body = "<html><body><pre>" + json_content + "</pre></body></html>"
         msg = MIMEText(msg_body, 'html', 'utf-8')
         msg['Subject'] = Header("2026", 'utf-8')
         msg['From'] = gmail_user
         msg['To'] = blogger_email
 
-        server = smtplib.SMTP('gmail.com', 587)
-        server.starttls() 
+        # ၄။ Gmail Server သို့ SSL ဖြင့် တိုက်ရိုက်ချိတ်ဆက်ခြင်း (ပိုမြန်ပါသည်)
+        server = smtplib.SMTP_SSL('gmail.com', 465)
         server.login(gmail_user, gmail_password)
         server.send_message(msg)
         server.quit()
