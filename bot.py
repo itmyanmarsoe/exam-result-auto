606e5c8da0855d4a43950285335ab409ff2ef2d0
itmyanmarsoe <148292993+itmyanmarsoe@users.noreply.github.com>
2026-04-18 16:15:12 +0630

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,15 +7,8 @@ pdf_url = "https://raw.githubusercontent.com/itmyanmarsoe/exam-result-auto/main/
 data = {"years": [{"name": "၂၀၂၆ အောင်စာရင်း (စမ်းသပ်ချက်)", "href": pdf_url}]}
 json_body = json.dumps(data, ensure_ascii=False)
 final_url = "googleapis.com" + str(BID) + "/posts"
-payload = {
-"kind": "blogger#post",
-"title": "2026",
-"content": "" + json_body + ""
-}
+payload = {"kind": "blogger#post", "title": "2026", "content": "" + json_body + ""}
 print("Blogger ဆီသို့ ပို့နေပါပြီ...")
 r = requests.post(final_url, params={'key': AKEY}, json=payload, timeout=30)
-if r.status_code == 200:
-print("အောင်မြင်စွာ တင်ပြီးပါပြီ။ Blog ကို စစ်ကြည့်ပါရှင်။")
-else:
 print("Error Code: " + str(r.status_code))
 print("Message: " + r.text)
